a147f16c6241c1eeca83c388b96ba536fc7d7459
Makitey <weldemichaelmakda@gmail.com>
2023-12-06 12:20:39 -0800

    101-square_matrix_map.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    average = 0
+    totalWeight = 0
+    totalScore = 0
+    if (len(my_list) <= 0):
+        return (0)
+    for item in my_list:
+        score, weight = item
+        totalWeight += weight
+        totalScore += score * weight
+    return totalScore / totalWeight
---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def square_matrix_map(matrix=[]):
+    return list(map(lambda y: list(map(lambda x: x ** 2, y)), matrix))
